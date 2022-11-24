3e9d2c36b26e4828dc368a50018801daafb54497
unknown <ulanovnikolay10p@gmail.com>
2022-11-24 15:56:48 +0300

    with right directory

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-const dist = "C:/openserver/domains/localhost/now";
+// const dist = "C:/openserver/domains/localhost/now";
+
+const dist = "./dist/";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
