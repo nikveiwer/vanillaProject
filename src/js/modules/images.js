5b0ad1de5267d67d631f0f7a2e8665a251c72949
unknown <ulanovnikolay10p@gmail.com>
2022-12-08 19:16:18 +0300

    i hop that's final

--- src/js/modules/images.js
+++ src/js/modules/images.js
@@ -1,32 +1,32 @@
 const images = () => {
-    const imgPopup = document.createElement("div");
-    const workSection = document.querySelector(".works");
-    const bigImg = document.createElement("img");
+    const imgPopup = document.createElement('div');
+    const workSection = document.querySelector('.works');
+    const bigImg = document.createElement('img');
 
-
-    imgPopup.classList.add("popup");
+    imgPopup.classList.add('popup');
     workSection.append(imgPopup);
 
-    imgPopup.style.justifyContent = "center";
-    imgPopup.style.alignItems = "center";
-    imgPopup.style.display = "none";
+    imgPopup.style.justifyContent = 'center';
+    imgPopup.style.alignItems = 'center';
+    imgPopup.style.display = 'none';
 
     imgPopup.append(bigImg);
 
-    workSection.addEventListener("click", (e) => {
+    workSection.addEventListener('click', (e) => {
         e.preventDefault();
 
         let target = e.target;
 
-        if (target && target.classList.contains("preview")) {
-            imgPopup.style.display = "flex";
-            const path = target.parentNode.getAttribute("href");
-            bigImg.setAttribute("src",path);
-            document.body.style.overflow = "hidden"
+        if (target && target.classList.contains('preview')) {
+            imgPopup.style.display = 'flex';
+            const path = target.parentNode.getAttribute('href');
+            bigImg.setAttribute('src', path);
+            document.body.style.overflow = 'hidden';
         }
 
-        if (target && target.matches("div.popup")) {
-            imgPopup.style.display = "none";
+        if (target && target.matches('div.popup')) {
+            imgPopup.style.display = 'none';
+            document.body.style.overflow = 'visible';
         }
     });
 };
